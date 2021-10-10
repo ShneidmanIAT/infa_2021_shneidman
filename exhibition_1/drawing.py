6c583f3b38f40f0d5a3a2c9908500c95cd589cc2
Shmyrkov <91532990+Shmyrkov@users.noreply.github.com>
2021-10-10 21:47:10 +0300

    Update drawing.py
    refactored

--- exhibition_1/drawing.py
+++ exhibition_1/drawing.py
@@ -1,105 +1,99 @@
 import random
-
 import pygame
 from pygame.draw import *
+
 pygame.init()
 sc = pygame.display.set_mode((800, 800))
+
 blue = (0, 0, 150)
 brown = (160, 82, 45)
 dogBrown = (80, 62, 25)
 green = (0, 200, 0)
 black = (0, 0, 0)
 white = (255, 255, 255)
+
 rect(sc, blue, (0, 0, 800, 400))
 rect(sc, green, (0, 400, 800, 400))
 
 
-def house(posx, posy, s):
-    s1 = pygame.Surface((400 * s, 200 * s)).convert_alpha()
+def draw_house(coordinate_x, coordinate_y, size):
+    s1 = pygame.Surface((400 * size, 200 * size)).convert_alpha()
     s1.fill([0, 0, 0, 0])
-    polygon(s1, brown, [[200 * s, 50 * s], [200 * s, 150 * s], [300 * s, 200 * s], [300 * s, 80 * s]])
-    polygon(s1, brown, [[200 * s, 50 * s], [250 * s, 30 * s], [300 * s, 80 * s]])
-    polygon(s1, brown, [[350 * s, 0 * s], [250 * s, 30 * s], [300 * s, 80 * s], [399 * s, 50 * s]])
-    polygon(s1, brown, [[300 * s, 200 * s], [300 * s, 80 * s], [399 * s, 50 * s], [399 * s, 130 * s]])
-    polygon(s1, black, [[200 * s, 50 * s], [200 * s, 150 * s], [300 * s, 200 * s], [300 * s, 80 * s]], 1)
-    polygon(s1, black, [[200 * s, 50 * s], [250 * s, 30 * s], [300 * s, 80 * s]], 1 * s)
-    polygon(s1, black, [[350 * s, 0 * s], [250 * s, 30 * s], [300 * s, 80 * s], [399 * s, 50 * s]], 1)
-    polygon(s1, black, [[300 * s, 200 * s], [300 * s, 80 * s], [399 * s, 50 * s], [399 * s, 130 * s]], 1)
-    circle(s1, black, (250 * s, 120 * s), 25 * s)
-    ychain = 120 * s
-    xchain = 208 * s
+    polygon(s1, brown, [[200 * size, 50 * size], [200 * size, 150 * size], [300 * size, 200 * size], [300 * size, 80 * size]])
+    polygon(s1, brown, [[200 * size, 50 * size], [250 * size, 30 * size], [300 * size, 80 * size]])
+    polygon(s1, brown, [[350 * size, 0 * size], [250 * size, 30 * size], [300 * size, 80 * size], [399 * size, 50 * size]])
+    polygon(s1, brown, [[300 * size, 200 * size], [300 * size, 80 * size], [399 * size, 50 * size], [399 * size, 130 * size]])
+    polygon(s1, black, [[200 * size, 50 * size], [200 * size, 150 * size], [300 * size, 200 * size], [300 * size, 80 * size]], 1)
+    polygon(s1, black, [[200 * size, 50 * size], [250 * size, 30 * size], [300 * size, 80 * size]], 1 * size)
+    polygon(s1, black, [[350 * size, 0 * size], [250 * size, 30 * size], [300 * size, 80 * size], [399 * size, 50 * size]], 1)
+    polygon(s1, black, [[300 * size, 200 * size], [300 * size, 80 * size], [399 * size, 50 * size], [399 * size, 130 * size]], 1)
+    circle(s1, black, (250 * size, 120 * size), 25 * size)
+    ychain = 120 * size
+    xchain = 208 * size
     for i in range(15):
-        ellipse(s1, black, (xchain, ychain, 20 * s, random.randint(7, 13) * s), 1)
-        xchain += -14 * s + random.randint(0, 4) * s
-        ychain += 3 * s + random.randint(-3, 3) * s
-    sc.blit(s1, (posx, posy))
+        ellipse(s1, black, (xchain, ychain, 20 * size, random.randint(7, 13) * size), 1)
+        xchain += -14 * size + random.randint(0, 4) * size
+        ychain += 3 * size + random.randint(-3, 3) * size
+    sc.blit(s1, (coordinate_x, coordinate_y))
     pygame.display.update()
 
 
-def fence(posx, posy, s, frequency):
-    s2 = pygame.Surface((800*s, 300*s)).convert_alpha()
+def draw_fence(coordinate_x, coordinate_y, size, frequency):
+    s2 = pygame.Surface((800*size, 300*size)).convert_alpha()
     s2.fill([0, 0, 0, 0])
-    rect(s2, brown, (0, 0, 800*s, 300*s))
-    line(s2, black, (0, 0), (800*s, 0), 2)
-    line(s2, black, (0, 298*s), (800*s, 298*s), 2)
+    rect(s2, brown, (0, 0, 800*size, 300*size))
+    line(s2, black, (0, 0), (800*size, 0), 2)
+    line(s2, black, (0, 298*size), (800*size, 298*size), 2)
     pos = 10
-    while pos < 800*s:
-        line(s2, black, (pos, 0), (pos,  300*s), 2)
+    while pos < 800*size:
+        line(s2, black, (pos, 0), (pos,  300*size), 2)
         pos += frequency
-    sc.blit(s2, (posx, posy))
+    sc.blit(s2, (coordinate_x, coordinate_y))
     pygame.display.update()
 
 
-def dog(posx, posy, s, a):
-    s3 = pygame.Surface((210 * s, 200 * s)).convert_alpha()
+def draw_dog(coordinate_x, coordinate_y, size):
+    s3 = pygame.Surface((210 * size, 200 * size)).convert_alpha()
     s3.fill([0, 0, 0, 0])
-    ellipse(s3, dogBrown, (0, 160*s, 30*s, 15*s))
-    ellipse(s3, dogBrown, (60 * s, 185*s, 30 * s, 15 * s))
-    ellipse(s3, dogBrown, (180 * s, 150 * s, 20 * s, 10 * s))
-    ellipse(s3, dogBrown, (130 * s, 125*s, 20 * s, 10 * s))
-    ellipse(s3, dogBrown, (10 * s, 85 * s, 30 * s, 80 * s))
-    ellipse(s3, dogBrown, (70 * s, 110 * s, 30 * s, 80 * s))
-    ellipse(s3, dogBrown, (190 * s, 110 * s, 20 * s, 45 * s))
-    ellipse(s3, dogBrown, (140 * s, 85 * s, 20 * s, 45 * s))
-    circle(s3, dogBrown, (135 * s, 75 * s), 25*s)
-    circle(s3, dogBrown, (185 * s, 100 * s), 25 * s)
-    ellipse(s3, dogBrown, (10 * s, 70 * s, 150 * s, 70 * s))
-    ellipse(s3, dogBrown, (110 * s, 50 * s, 90 * s, 60 * s))
-    rect(s3, dogBrown, (16 * s, 1 * s, 85 * s, 85 * s))
-    rect(s3, black, (16 * s, 1 * s, 85 * s, 85 * s), 1)
-    ellipse(s3, dogBrown, (0, 0, 20 * s, 20 * s))
-    ellipse(s3, black, (0, 0, 20 * s, 20 * s), 1)
-    ellipse(s3, dogBrown, (97 * s, 0, 20 * s, 20 * s))
-    ellipse(s3, black, (97 * s, 0, 20 * s, 20 * s), 1)
-    ellipse(s3, white, (30 * s, 16 * s, 15 * s, 10 * s))
-    ellipse(s3, black, (30 * s, 16 * s, 15 * s, 10 * s), 1)
-    ellipse(s3, white, (30 * s, 16 * s, 15 * s, 10 * s))
-    ellipse(s3, black, (30 * s, 16 * s, 15 * s, 10 * s), 1)
-    ellipse(s3, white, (72 * s, 16 * s, 15 * s, 10 * s))
-    ellipse(s3, black, (72 * s, 16 * s, 15 * s, 10 * s), 1)
-    circle(s3, black, (79.5 * s, 21 * s), 4*s)
-    circle(s3, black, (37.5 * s, 21 * s), 4*s)
-    polygon(s3, white, ((50 * s, 45 * s), (55 * s, 55 * s), (45 * s, 55 * s)))
-    polygon(s3, white, ((67 * s, 45 * s), (72 * s, 55 * s), (62 * s, 55 * s)))
-    polygon(s3, black, ((50 * s, 45 * s), (55 * s, 55 * s), (45 * s, 55 * s)), 1)
-    polygon(s3, black, ((67 * s, 45 * s), (72 * s, 55 * s), (62 * s, 55 * s)), 1)
-    ellipse(s3, dogBrown, (26 * s, 53 * s, 65 * s, 30 * s))
-    ellipse(s3, black, (26 * s, 53 * s, 65 * s, 30 * s), 1)
-    rect(s3, dogBrown, (17 * s, 65 * s, 83 * s, 19 * s))
-    s3 = pygame.transform.flip(s3, a, False)
-    sc.blit(s3, (posx, posy))
+    ellipse(s3, dogBrown, (0, 160*size, 30*size, 15*size))
+    ellipse(s3, dogBrown, (60 * size, 185*size, 30 * size, 15 * size))
+    ellipse(s3, dogBrown, (180 * size, 150 * size, 20 * size, 10 * size))
+    ellipse(s3, dogBrown, (130 * size, 125*size, 20 * size, 10 * size))
+    ellipse(s3, dogBrown, (10 * size, 85 * size, 30 * size, 80 * size))
+    ellipse(s3, dogBrown, (70 * size, 110 * size, 30 * size, 80 * size))
+    ellipse(s3, dogBrown, (190 * size, 110 * size, 20 * size, 45 * size))
+    ellipse(s3, dogBrown, (140 * size, 85 * size, 20 * size, 45 * size))
+    circle(s3, dogBrown, (135 * size, 75 * size), 25*size)
+    circle(s3, dogBrown, (185 * size, 100 * size), 25 * size)
+    ellipse(s3, dogBrown, (10 * size, 70 * size, 150 * size, 70 * size))
+    ellipse(s3, dogBrown, (110 * size, 50 * size, 90 * size, 60 * size))
+    rect(s3, dogBrown, (16 * size, 1 * size, 85 * size, 85 * size))
+    rect(s3, black, (16 * size, 1 * size, 85 * size, 85 * size), 1)
+    ellipse(s3, dogBrown, (0, 0, 20 * size, 20 * size))
+    ellipse(s3, black, (0, 0, 20 * size, 20 * size), 1)
+    ellipse(s3, dogBrown, (97 * size, 0, 20 * size, 20 * size))
+    ellipse(s3, black, (97 * size, 0, 20 * size, 20 * size), 1)
+    ellipse(s3, white, (30 * size, 16 * size, 15 * size, 10 * size))
+    ellipse(s3, black, (30 * size, 16 * size, 15 * size, 10 * size), 1)
+    ellipse(s3, white, (30 * size, 16 * size, 15 * size, 10 * size))
+    ellipse(s3, black, (30 * size, 16 * size, 15 * size, 10 * size), 1)
+    ellipse(s3, white, (72 * size, 16 * size, 15 * size, 10 * size))
+    ellipse(s3, black, (72 * size, 16 * size, 15 * size, 10 * size), 1)
+    circle(s3, black, (79.5 * size, 21 * size), 4*size)
+    circle(s3, black, (37.5 * size, 21 * size), 4*size)
+    polygon(s3, white, ((50 * size, 45 * size), (55 * size, 55 * size), (45 * size, 55 * size)))
+    polygon(s3, white, ((67 * size, 45 * size), (72 * size, 55 * size), (62 * size, 55 * size)))
+    polygon(s3, black, ((50 * size, 45 * size), (55 * size, 55 * size), (45 * size, 55 * size)), 1)
+    polygon(s3, black, ((67 * size, 45 * size), (72 * size, 55 * size), (62 * size, 55 * size)), 1)
+    ellipse(s3, dogBrown, (26 * size, 53 * size, 65 * size, 30 * size))
+    ellipse(s3, black, (26 * size, 53 * size, 65 * size, 30 * size), 1)
+    rect(s3, dogBrown, (17 * size, 65 * size, 83 * size, 19 * size))
+    sc.blit(s3, (coordinate_x, coordinate_y))
     pygame.display.update()
 
-
-fence(0, 200, 0.5, 20)
-house(350, 450, 1)
-dog(70, 501, 1.2, False)
-fence(400, 100, 0.7, 10)
-fence(200, 300, 0.5, 10)
-fence(0, 400, 0.3, 10)
-dog(400, 450, 1, True)
-dog(550, 550, 3, False)
-
+draw_fence(0, 200, 1, 20)
+draw_house(350, 450, 1)
+draw_dog(70, 501, 1.2)
 
 while True:
     for event in pygame.event.get():
