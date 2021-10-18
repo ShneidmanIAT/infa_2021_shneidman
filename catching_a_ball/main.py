0ac016bf6a65eef46b1f834580b2ead1a044d2dd
ShneidmanIAT <91080676+ShneidmanIAT@users.noreply.github.com>
2021-10-18 20:39:32 +0300

    patch of game with circles
    fixed the colours and added score

--- catching_a_ball/main.py
+++ catching_a_ball/main.py
@@ -7,29 +7,55 @@ from pygame.draw import *
 FPS = 30
 pygame.init()
 screen = pygame.display.set_mode((400, 400))
-numofelements = 2
+Amount_of_balls = 4
 
-def handle_events(x, y, finished):
+
+def handle_events():
+    """
+    gets the tap on screen or on the cross
+    """
+    finish_clicked = False
+    x = screen.get_width() + 1
+    y = screen.get_height() + 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            finished = True
+            finish_clicked = True
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
-    return(finished, x, y)
+    return(finish_clicked, x, y)
 
 
-def game_process(xcirc, ycirc, x, y, rad, score, ball_speed):
+def game_process(xcirc, ycirc, xclick, yclick, rad, score, ball_speed, colour):
+    """
+    matches clikced coords with coords of every ball and moves/replaces them
+    :param xcirc: array of x coordinates of balls
+    :param ycirc: array of y coordinates of balls
+    :param xclick: x of click
+    :param yclick: y of click
+    :param rad: array of rads of balls
+    :param score: score of playa
+    :param ball_speed: array of x and y speeds  of balls
+    :param colour: array of colours of balls
+    :return:
+    """
     for i in range(len(xcirc)):
-        if (x - xcirc[i])**2 + (y-ycirc[i])**2 < rad[i]**2:
+        if (xclick - xcirc[i])**2 + (yclick - ycirc[i])**2 < rad[i]**2:
             score += 10
-            print(score)
-            xcirc[i], ycirc[i], rad[i], ball_speed[i] = new_ball(xcirc[i], ycirc[i], rad[i], ball_speed[i])
+            xcirc[i], ycirc[i], rad[i], ball_speed[i], colour[i] = new_ball(xcirc[i], ycirc[i], rad[i], ball_speed[i])
     for i in range(len(xcirc)):
-        xcirc[i], ycirc[i], rad[i], ball_speed[i] = move_ball(xcirc[i], ycirc[i], rad[i], ball_speed[i])
-    return (xcirc, ycirc, rad, score, ball_speed)
+        xcirc[i], ycirc[i], rad[i], ball_speed[i], colour[i] = move_ball(xcirc[i], ycirc[i], rad[i], ball_speed[i], colour[i])
+    return (xcirc, ycirc, rad, score, ball_speed, colour)
 
 
-def move_ball(xcirc, ycirc, rad, ball_speed):
+def move_ball(xcirc, ycirc, rad, ball_speed, colour):
+    """
+    moves a ball on parameters tied with speed
+    :param xcirc: x coord of moved ball
+    :param ycirc: y coord of moved ball
+    :param rad: rad coord of moved ball
+    :param ball_speed: ball speed array of moved ball
+    :param colour: colour array of moved ball
+    """
     circle(screen, (0, 0, 0), (xcirc, ycirc), rad)
     if xcirc + ball_speed[0] > 400 and xcirc > 0:
         ball_speed[0] = -ball_speed[0]
@@ -41,48 +67,69 @@ def move_ball(xcirc, ycirc, rad, ball_speed):
         ball_speed[1] = -ball_speed[1]
     ycirc += ball_speed[1]
     xcirc += ball_speed[0]
-    circle(screen, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (xcirc, ycirc), rad)
-    return (xcirc, ycirc, rad, ball_speed)
+    circle(screen, colour, (xcirc, ycirc), rad)
+    return (xcirc, ycirc, rad, ball_speed, colour)
 
 
 def new_ball(xcirc, ycirc, rad, ball_speed):
+    """
+    replaces clicked ball on screen with new with random parametres
+    :param xcirc: x coord of replaced ball
+    :param ycirc: y coord of replaced ball
+    :param rad: rad coord of replaced ball
+    :param ball_speed: ball speed array of replaced ball
+    :return:
+    """
     circle(screen, (0, 0, 0), (xcirc, ycirc), rad)
     ycirc = random.randint(50, 350)
     xcirc = random.randint(50, 350)
     rad = random.randint(12, 50)
     ball_speed[0] = random.randint(-10, 15)
     ball_speed[1] = random.randint(-10, 10)
-    circle(screen, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), (xcirc, ycirc), rad)
-    return xcirc, ycirc, rad, ball_speed
+    colour = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
+    circle(screen, colour, (xcirc, ycirc), rad)
+    return xcirc, ycirc, rad, ball_speed, colour
 
 
-ycirc = [None] * numofelements
-xcirc = [None] * numofelements
-rad = [None] * numofelements
+def score_to_screen(score):
+    """
+    draws score on screen
+    :param score: score of player
+    """
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render(str(score), False, (0, 180, 0))
+    rect(screen, (0, 0, 0), (0, 0, 200, 200))
+    screen.blit(textsurface, (10, 10))
+
+
+ycirc = [None] * Amount_of_balls
+xcirc = [None] * Amount_of_balls
+rad = [None] * Amount_of_balls
 ball_speed = []
-for i in range(numofelements):
+colour = []
+for i in range(Amount_of_balls):
     ycirc[i] = random.randint(20, 380)
     xcirc[i] = random.randint(20, 380)
     rad[i] = random.randint(12, 50)
     ball_speed.append([random.randint(10,50), random.randint(10, 50)])
+    colour.append([0, 0, 0])
 
 for i in range(len(xcirc)):
-    xcirc[i], ycirc[i], rad[i], ball_speed[i] = new_ball(ycirc[i], xcirc[i], rad[i], ball_speed[i])
+    xcirc[i], ycirc[i], rad[i], ball_speed[i], colour[i] = new_ball(ycirc[i], xcirc[i], rad[i], ball_speed[i])
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-x = 0
-y = 0
+pygame.font.init()
+xclick = 0
+yclick = 0
 score = 0
-
 while not finished:
     clock.tick(FPS)
-    finished, x, y = handle_events(x, y, finished)
-    xcirc, ycirc, rad, score, ball_speed = game_process(xcirc, ycirc, x, y, rad, score, ball_speed)
-    x = 800
-    y = 800
+    score_to_screen(score)
+    finished, xclick, yclick = handle_events()
+    xcirc, ycirc, rad, score, ball_speed, colour = game_process(xcirc, ycirc, xclick, yclick, rad, score, ball_speed, colour)
+    xclick = screen.get_width() + 1
+    yclick = screen.get_height() + 1
     pygame.display.update()
-
-
 pygame.quit()
